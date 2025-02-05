aba699cd568df47d40ddcebf93a6302eb11c989e
Joshua Tazman Reinier <jtreinier@gmail.com>
2025-02-05 11:51:05 -0500

    commit

--- controls/src/app/page.tsx
+++ controls/src/app/page.tsx
@@ -1,5 +1,7 @@
-import Image from 'next/image'
+'use client'
+
+import { AsemicCanvas } from '@/asemic/src/Asemic'
 
 export default function Home() {
-  return <></>
+  return <AsemicCanvas></AsemicCanvas>
 }
