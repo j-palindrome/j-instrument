e707710a0a1ccc086184855d7b8aadcd9bff9894
Joshua Tazman Reinier <jtreinier@gmail.com>
2025-02-05 12:48:45 -0500

    controls v2

--- controls/src/app/page.tsx
+++ controls/src/app/page.tsx
@@ -1,7 +1,25 @@
 'use client'
-
-import { AsemicCanvas } from '@/asemic/src/Asemic'
+import Image from 'next/image'
+import { AsemicCanvas, useAsemic } from '../../asemic/src/Asemic'
+import LineBrush from '../../asemic/src/LineBrush'
+import DashBrush from '../../asemic/src/DashBrush'
 
 export default function Home() {
-  return <AsemicCanvas></AsemicCanvas>
+  return (
+    <AsemicCanvas>
+      <Scene></Scene>
+    </AsemicCanvas>
+  )
+}
+
+function Scene() {
+  const { h } = useAsemic()
+  console.log(h)
+
+  return (
+    <>
+      <LineBrush
+        onInit={b => b.newCurve([0, 0, { thickness: 1 }], [1, h])}></LineBrush>
+    </>
+  )
 }
