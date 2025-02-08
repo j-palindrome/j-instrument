e43d40436babed3c2ce8064242224d3e8c124170
Jay Reinier <jtreinier@gmail.com>
2025-02-07 23:31:13 -0500

    add osc server

---
+++ controls/src/server/index.ts
@@ -0,0 +1,149 @@
+import { ip } from 'address'
+//e.g server.js
+
+import express from 'express'
+import fs from 'node:fs'
+import path from 'node:path'
+import { Server as SocketServer } from 'socket.io'
+import ViteExpress from 'vite-express'
+import { Client, Server } from 'node-osc'
+
+const app = express()
+
+const server = ViteExpress.listen(app, 7001, () =>
+  console.log('Server is listening...')
+)
+
+// And then attach the socket.io server to the HTTP server
+const io = new SocketServer<SocketEvents>(server)
+
+// Create an OSC server
+const oscServer = new Server(7000, 'localhost', () => {
+  console.log('OSC Server is listening on port 7000')
+})
+
+// Create an OSC client
+const oscClient = new Client('localhost', 7001)
+oscClient.send(['thing', 200])
+// Handle incoming OSC messages
+oscServer.on('message', msg => {
+  console.log(`Received OSC message: ${msg}`)
+  // You can handle the message and emit events to the socket.io clients if needed
+  io.emit('oscMessage', msg)
+})
+
+// Example of sending an OSC message
+const sendOscMessage = (address: string, ...args: any[]) => {
+  oscClient.send(address, ...args, err => {
+    if (err) {
+      console.error('Error sending OSC message:', err)
+    } else {
+      console.log(`OSC message sent to ${address} with args: ${args}`)
+    }
+  })
+}
+
+// Then you can use `io` to listen the `connection` event and get a socket
+// from a client
+
+const settings: {
+  mediaFolder: string
+} = JSON.parse(
+  fs.readFileSync(path.resolve(process.cwd(), 'settings.json')).toString()
+)
+
+const updateSettings = (newSettings: Partial<typeof settings>) => {
+  for (let key of Object.keys(newSettings)) {
+    settings[key] = newSettings[key]
+  }
+  fs.writeFileSync(
+    path.resolve(process.cwd(), 'settings.json'),
+    JSON.stringify(settings)
+  )
+}
+
+const ipAdd = ip()
+oscClient.send([
+  '/message',
+  `Go to http://${ipAdd}:7001 from an iPad signed into same WiFi to access UI.`
+])
+oscClient.send(['/message/ip', `http://${ipAdd}:7001`])
+oscClient.send([
+  '/message/name',
+  `name`,
+  `presets_${new Date().toISOString().slice(0, 10)}.json`
+])
+
+io.on('connection', socket => {
+  socket.on('set', (route: string, property: string, value: any) => {
+    if (value instanceof Array) {
+      oscClient.send([route, property, ...value])
+    } else {
+      if ((property === 'file1' || property === 'file2') && value) {
+        value = path.resolve(settings.mediaFolder, value)
+      }
+      oscClient.send([route, property, value])
+    }
+  })
+
+  const presetsPath = path.resolve(process.cwd(), 'presets.json')
+  if (!fs.existsSync(presetsPath)) {
+    fs.writeFileSync(presetsPath, '{}')
+  }
+  let presets = fs.readFileSync(presetsPath).toString()
+
+  socket.on('loadPresets', callback => {
+    callback(presets)
+  })
+
+  socket.on('savePresets', presets => {
+    fs.promises.writeFile(
+      path.resolve(process.cwd(), 'presets.json'),
+      JSON.stringify(presets)
+    )
+  })
+
+  const readFiles = () => {
+    if (!settings.mediaFolder) return
+    try {
+      const files = fs
+        .readdirSync(settings.mediaFolder)
+        .filter(file =>
+          /\.(mov|mp4|m4a|png|jpg|aif|gif|webm|webp|vlc)$/.test(file)
+        )
+      socket.emit('setFiles', files)
+    } catch (err) {}
+  }
+  oscServer.addListener('/setMediaFolder', (folder: string) => {
+    try {
+      updateSettings({ mediaFolder: folder })
+      readFiles()
+    } catch (err) {
+      updateSettings({ mediaFolder: '' })
+    }
+  })
+
+  oscServer.addListener('setPresetsFile', (file: string) => {
+    try {
+      const fileContents = fs.readFileSync(file, 'utf-8')
+      fs.writeFileSync(
+        path.resolve(process.cwd(), 'presets.json'),
+        fileContents
+      )
+      socket.emit('setPresets', fileContents)
+    } catch (err) {
+      console.log('failed')
+    }
+  })
+  readFiles()
+
+  oscServer.addListener('/savePresetsFile', (file: string) => {
+    socket.emit('getPresets', presets => {
+      fs.promises.writeFile(file, presets)
+    })
+  })
+
+  oscServer.addListener('/spaceMouse', (...data) => {
+    socket.emit('getSpaceMouse', data.slice(0, 3), data.slice(3, 6))
+  })
+})
