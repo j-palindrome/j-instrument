42a7dcbdb15a2229441ed1e5ca3d12fc6fb086ff
Jay Reinier <jtreinier@gmail.com>
2025-02-10 16:37:02 -0500

    move over OscFrame

--- controls/src/App.tsx
+++ controls/src/App.tsx
@@ -1,138 +1,95 @@
 import { now } from 'lodash'
-import { useEffect, useState } from 'react'
-import { io } from 'socket.io-client'
+import { useState } from 'react'
 import { AsemicCanvas, useAsemic } from '../../libs/asemic/src/Asemic'
 import LineBrush from '../../libs/asemic/src/LineBrush'
+import { useSocket } from '../../libs/oscPresets/components/context'
+import { OscFrame } from '../../libs/oscPresets/components/OscFrame'
 import Button from './components/Button'
-import { SocketProvider, useSocket } from './context'
-import { PresetSocket, setters } from './store'
 
 function App() {
-  const [socket, setSocket] = useState<PresetSocket>()
-
-  useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then(devices => {
-      const audioInputs = devices.filter(device => device.kind === 'audioinput')
-      console.log('Available audio inputs:', audioInputs)
-    })
-  }, [])
-
-  useEffect(() => {
-    const socket: PresetSocket = io()
-    setSocket(socket)
-
-    socket.emit('load', presets => {
-      // for (let value of Object.values(presets)) {
-      //   for (let key of defaultKeys) {
-      //     if (value[key] === undefined) {
-      //       value[key] = initialGlobal[key]
-      //     }
-      //   }
-      // }
-
-      setters.set({
-        presets
-      })
-    })
-
-    return () => {
-      socket.close()
-    }
-  }, [])
+  const socket = useSocket()
   const [lastRecord, setLastRecord] = useState(0)
-
   return (
-    socket && (
-      <SocketProvider socket={socket}>
-        <>
-          <div>
-            <Button
-              label='record'
-              cb={state => {
-                if (state) {
-                  const nowStr = now()
+    <OscFrame>
+      <div>
+        <Button
+          label='record'
+          cb={state => {
+            if (state) {
+              const nowStr = now()
+              socket.emit(
+                'get',
+                'path',
+                {
+                  relativePath: `../exports`
+                },
+                path => {
                   socket.emit(
-                    'get',
-                    'path',
-                    {
-                      relativePath: `../exports`
-                    },
-                    path => {
-                      socket.emit(
-                        'osc',
-                        'td',
-                        '/record/filename',
-                        path + `/${nowStr}.mov`
-                      )
-                      socket.emit(
-                        'osc',
-                        'max',
-                        '/record/filename',
-                        `open`,
-                        `${path}/${nowStr}.wav`
-                      )
-                      setLastRecord(nowStr)
-                      window.setTimeout(
-                        () => socket.emit('osc', 'all', '/record/status', 1),
-                        500
-                      )
-                    }
+                    'osc',
+                    'td',
+                    '/record/filename',
+                    path + `/${nowStr}.mov`
                   )
-                } else {
-                  console.log('stop')
-                  socket.emit('osc', 'all', '/record/status', 0)
-                  socket.emit('do', 'encode', {
-                    timestamp: lastRecord
-                  })
+                  socket.emit(
+                    'osc',
+                    'max',
+                    '/record/filename',
+                    `open`,
+                    `${path}/${nowStr}.wav`
+                  )
+                  setLastRecord(nowStr)
+                  window.setTimeout(
+                    () => socket.emit('osc', 'all', '/record/status', 1),
+                    500
+                  )
                 }
-              }}
-            />
-          </div>
-          <AsemicCanvas
-            useAudio
-            outputChannel={ctx => {
-              switch (ctx.destination.maxChannelCount) {
-                case 32:
-                  console.log('32 outputs')
+              )
+            } else {
+              console.log('stop')
+              socket.emit('osc', 'all', '/record/status', 0)
+              socket.emit('do', 'encode', {
+                timestamp: lastRecord
+              })
+            }
+          }}
+        />
+      </div>
+      <AsemicCanvas
+        useAudio
+        outputChannel={ctx => {
+          switch (ctx.destination.maxChannelCount) {
+            case 32:
+              console.log('32 outputs')
 
-                  // MOTU
-                  socket.emit(
-                    'osc',
-                    'max',
-                    '/channels/device',
-                    'BlackHole Ultralite'
-                  )
-                  socket.emit('osc', 'max', '/channels/input', 1, 2)
-                  socket.emit('osc', 'max', '/channels/output', 17, 18)
-                  socket.emit('osc', 'max', '/channels/loopback', 15, 16)
-                  // // the recording loopback from Max to TD
-                  socket.emit('osc', 'td', '/channels/loopback', 32, 33)
-                  return 18
-                case 18:
-                  // MacBook
-                  socket.emit(
-                    'osc',
-                    'max',
-                    '/channels/device',
-                    'Mac & BlackHole'
-                  )
-                  socket.emit('osc', 'max', '/channels/input', 2, 3)
-                  socket.emit('osc', 'max', '/channels/output', 1, 2)
-                  // the recording loopback from Max to TD
-                  socket.emit('osc', 'max', '/channels/loopback', 17, 18)
-                  socket.emit('osc', 'td', '/channels/loopback', 15, 16)
-                  return 2
-                default:
-                  throw new Error(
-                    'must use BlackHole with MOTU or with Mac Audio'
-                  )
-              }
-            }}>
-            <Scene />
-          </AsemicCanvas>
-        </>
-      </SocketProvider>
-    )
+              // MOTU
+              socket.emit(
+                'osc',
+                'max',
+                '/channels/device',
+                'BlackHole Ultralite'
+              )
+              socket.emit('osc', 'max', '/channels/input', 1, 2)
+              socket.emit('osc', 'max', '/channels/output', 17, 18)
+              socket.emit('osc', 'max', '/channels/loopback', 15, 16)
+              // // the recording loopback from Max to TD
+              socket.emit('osc', 'td', '/channels/loopback', 32, 33)
+              return 18
+            case 18:
+              // MacBook
+              socket.emit('osc', 'max', '/channels/device', 'Mac & BlackHole')
+              socket.emit('osc', 'max', '/channels/input', 2, 3)
+              socket.emit('osc', 'max', '/channels/output', 1, 2)
+              // the recording loopback from Max to TD
+              socket.emit('osc', 'max', '/channels/loopback', 17, 18)
+              socket.emit('osc', 'td', '/channels/loopback', 15, 16)
+              return 2
+            default:
+              throw new Error('must use BlackHole with MOTU or with Mac Audio')
+          }
+        }}>
+        <Scene />
+      </AsemicCanvas>
+    </OscFrame>
   )
 }
 
